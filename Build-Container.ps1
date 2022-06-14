ee5f8a54ff41b6bd9ea917dac00fbffa0f45e693
Yilmaz MUSTAFA <yilmaz.mustafa@intecbrussel.be>
2022-06-14 09:59:54 +0200

    docker remove if the container exists

--- Build-Container.ps1
+++ Build-Container.ps1
@@ -2,12 +2,14 @@ mvn clean install
 docker build -t yilmazchef/kaartje:latest .
 docker run `
         --publish 3306:3306 `
+        --rm `
         --env MYSQL_ROOT_PASSWORD=toor `
         --env MYSQL_USER=intec `
         --env MYSQL_PASSWORD=intec `
         --env MYSQL_DATABASE=db `
         --detach mysql:latest
 docker run `
-        --publish 8080:8080 `
+        --publish 8888:8080 `
+        --rm `
         --env .\.env `
         --detach yilmazchef/kaartje:latest
